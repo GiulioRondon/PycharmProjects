44df12781025ca253e93e174ee201e88985349ed
Giulio <giulio.rondon@student.hu.nl>
2017-09-12 17:42:16 +0200

    o

---
+++ Practise Exercise 2/pe2_4.py
@@ -0,0 +1,6 @@
+uren = input('Hoevel uur heb je gewerkt? ')
+loon = input('Wat is je loon per uur? ')
+
+antw = float(loon) * int(uren)
+
+print(str(uren) + ' uur werken levert ' + str(antw) + ' euro op')
